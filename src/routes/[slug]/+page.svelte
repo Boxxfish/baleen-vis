32f0c7bd7b738e4f990fcfea37ff9c662826dd35
Ben Giacalone <bensgiacalone@gmail.com>
2024-03-02 20:01:48 -0500

    Add static rendering

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -8,7 +8,7 @@
 
 <svelte:head>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
   <link
     href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
     rel="stylesheet"
--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -1,13 +1,15 @@
 import type { PageLoad } from "./$types";
+import type { EntryGenerator } from "./$types";
+import type { LayoutParams, RouteParams } from "../$types";
 
 export interface HopData {
-  c_toks: string[],
-  q_matches: number[][], 
-  c_matches: number[][],
-  ranking: [number, String][],
-  stage1: [number, String][],
-  stage2: [number, String][],
-  new_ctx: String,
+  c_toks: string[];
+  q_matches: number[][];
+  c_matches: number[][];
+  ranking: [number, String][];
+  stage1: [number, String][];
+  stage2: [number, String][];
+  new_ctx: String;
 }
 
 export interface QueryData {
@@ -19,4 +21,11 @@ export const load: PageLoad<QueryData> = async ({ fetch, params }) => {
   const res = await fetch(`/data/${params.slug}.json`);
   const json = await res.json();
   return json;
+};
+
+export const entries: EntryGenerator = () => {
+  const arr = [...Array(10).keys()].map((i) => ({
+    slug: `${i}`,
+  }));
+  return arr;
 };
